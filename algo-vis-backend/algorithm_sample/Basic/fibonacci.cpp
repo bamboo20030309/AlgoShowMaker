97fd8213215f959b1d2f52d49211697364139c03
bamboo20030309 <bamboo20030309a@gmail.com>
2026-03-10 01:21:46 +0800

    feat:新增tree_layout的物件 與 遞迴費式數列的範例

--- algo-vis-backend/algorithm_sample/Basic/fibonacci.cpp
+++ algo-vis-backend/algorithm_sample/Basic/fibonacci.cpp
@@ -8,29 +8,36 @@ TreeLayout tree(2, Pos(500, 100), 10, 80.0, 120.0);
 //}
 
 int F(int n) {
+    //draw{
     int d = tree.curr_d;
     int o = tree.curr_o;
     
     tree.paint(av, "F(" + to_string(n) + ")"); 
-
+    //}
     if (n <= 1) {
+        //draw{
         tree.edge_colors[{d, o}] = "black";
         tree.paint(av, n, n, [&]{
             av.text("遇到擋板，回傳 " + to_string(n), Pos(tree.get_id(d, o), 0, "bottom", 0, 40));
         });
+        //}
         return n;
     }
-
+    //draw{
     tree.push(0); 
+    //}
     int a = F(n - 1);
+    //draw{
     tree.pop();
 
     tree.push(1); 
+    //}
     int b = F(n - 2);
+    //draw{
     tree.pop();
-
+    //}
     int res = a + b;
-    
+    //draw{
     tree.paint(av, to_string(a) + " + " + to_string(b), -1, [&]{
         av.text("合併結果", Pos(tree.get_id(d, o), 0, "bottom", 0, 40));
     }); 
@@ -39,22 +46,24 @@ int F(int n) {
     tree.paint(av, res, res, [&]{
         av.text("回傳 " + to_string(res), Pos(tree.get_id(d, o), 0, "bottom", 0, 40));
     }); 
+    //}
     
     return res;
 }
 
 int main() {
+    //draw{
     tree.renderer = [](string id, Pos p, int d, int o, bool is_focus) {
         string val = tree.vals[{d, o}];
         int res = tree.results.count({d, o}) ? tree.results[{d, o}] : -1;
 
-        //draw{
+        
         av.frame_draw(id, p, vector<string>{val}); 
         if(is_focus) av.camera(p, 1.3);
-        //}
+        
     };
-
-    int n = 5;
+    //}
+    int n; cin>>n;
     //draw{
     av.start_draw();
 
