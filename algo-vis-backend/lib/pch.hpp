1b6089c012a08b24af6a0cb312a591a5aa4ae6f4
bamboo20030309 <bamboo20030309a@gmail.com>
2026-04-02 10:26:18 +0800

    feat:新增pch.hpp 能夠預編譯各個大型標頭檔 省下大量編譯時間

---
+++ algo-vis-backend/lib/pch.hpp
@@ -0,0 +1,19 @@
+#ifndef PCH_HPP
+#define PCH_HPP
+
+// 包含所有常用的重量級標頭檔
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <tuple>
+
+// 包含您的核心引擎
+#include "AV.hpp"
+
+#endif // PCH_HPP
