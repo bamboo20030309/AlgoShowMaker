49e64ba6edc45c54fbb1b52551d7a5b8deac6948
bamboo20030309 <bamboo20030309a@gmail.com>
2026-04-13 21:21:06 +0800

    fix:修正預設範例

--- algo-vis-backend/public/front.js
+++ algo-vis-backend/public/front.js
@@ -308,19 +308,24 @@ fetch('sample_code.cpp')
     const fallbackCode = `#include <bits/stdc++.h>
 #include "AV.hpp"
 using namespace std;
+AV av;
 int main() {
-    vector<int> num;
-    AV av;
+    vector<int> num={0};
     av.start_draw();
     for (int i = 0; i < 20; i++) {
-        num.push_back(i);
+        num.push_back(i+1);
         av.start_frame_draw();
-        av.frame_draw("num", 0, 0, num, {i-1}, {i-1}, {0}, 0, "normal", 0, 1, false, false);
-        av.frame_draw("heap", 0, 150, num, {i-1}, {i-1}, {0}, 0, "heap", 10, 1, false, false);
-        if(i==3 || i==7) {
-            av.key_frame_draw("num", 0, 0, num, AV::AtoB(0,i), {i-1}, {0}, 0, "normal", 0, 1, false, false);
-            av.key_frame_draw("heap", 0, 150, num, AV::AtoB(0,i), {i-1}, {0}, 0, "heap", 10, 1, false, false);
+        av.frame_draw("num", Pos(0,0), num, {{{"highlight"},{i}}, {{"focus"},{i}}, {{"point"},{i}}, {{"mark"},{i}}, {{"background"},{i}}}, {0},  "normal", 0, 1);
+        av.frame_draw("heap", Pos("num","raw bottom-left",0,100), num, {{{"highlight"},{i-1}}, {{"focus"},{i-1}}, {{"point"},{i-1}}, {{"mark"},{i-1}}, {{"background"},{i-1}}}, {0},  "heap", 10, 1);
+        av.frame_draw("BIT", Pos("heap","raw bottom-left",0,100), num, {{{"highlight"},{i-1}}, {{"focus"},{i-1}}, {{"point"},{i-1}}, {{"mark"},{i-1}}, {{"background"},{i-1}}}, {0},  "BIT", 10, 1);
+        av.arrow( Pos("num","bottom"), Pos("heap","top"), {{"color","black"},{"width","3"}});
+        av.arrow( Pos("num",i+1), Pos("BIT",i));
+        if(i==3 || i==7 || i==13) {
+            av.key_frame_draw("num", Pos(0,0), num, {{{"mark"},AV::AtoB(0,i)}, {{"highlight"},{i}}, {{"point"},{i}}, {{"focus"},{i}}, {{"background"},{i}}}, {0},  "normal", 0, 1);
+            av.key_frame_draw("heap", Pos("num","raw bottom-left",0,100), num, {{{"mark"},AV::AtoB(0,i)}, {{"highlight"},{i-1}}, {{"point"},{i-1}}, {{"focus"},{i-1}}, {{"background"},{i-1}}}, {0},  "heap", 10, 1);
+            av.key_frame_draw("BIT", Pos("heap","raw bottom-left",0,100), num, {{{"mark"},AV::AtoB(0,i)}, {{"highlight"},{i-1}}, {{"point"},{i-1}}, {{"focus"},{i-1}}, {{"background"},{i-1}}}, {0},  "BIT", 10, 1);
         }
+        av.auto_camera();
         av.end_frame_draw();
     }
     av.end_draw();
--- algo-vis-backend/public/sample_code.cpp
+++ algo-vis-backend/public/sample_code.cpp
@@ -9,15 +9,16 @@ int main() {
         num.push_back(i+1);
         av.start_frame_draw();
         av.frame_draw("num", Pos(0,0), num, {{{"highlight"},{i}}, {{"focus"},{i}}, {{"point"},{i}}, {{"mark"},{i}}, {{"background"},{i}}}, {0},  "normal", 0, 1);
-        av.frame_draw("heap", Pos("num","bottom-left",0,100), num, {{{"highlight"},{i-1}}, {{"focus"},{i-1}}, {{"point"},{i-1}}, {{"mark"},{i-1}}, {{"background"},{i-1}}}, {0},  "heap", 10, 1);
-        av.frame_draw("BIT", Pos("heap","bottom-left",0,100), num, {{{"highlight"},{i-1}}, {{"focus"},{i-1}}, {{"point"},{i-1}}, {{"mark"},{i-1}}, {{"background"},{i-1}}}, {0},  "BIT", 10, 1);
+        av.frame_draw("heap", Pos("num","raw bottom-left",0,100), num, {{{"highlight"},{i-1}}, {{"focus"},{i-1}}, {{"point"},{i-1}}, {{"mark"},{i-1}}, {{"background"},{i-1}}}, {0},  "heap", 10, 1);
+        av.frame_draw("BIT", Pos("heap","raw bottom-left",0,100), num, {{{"highlight"},{i-1}}, {{"focus"},{i-1}}, {{"point"},{i-1}}, {{"mark"},{i-1}}, {{"background"},{i-1}}}, {0},  "BIT", 10, 1);
         av.arrow( Pos("num","bottom"), Pos("heap","top"), {{"color","black"},{"width","3"}});
         av.arrow( Pos("num",i+1), Pos("BIT",i));
         if(i==3 || i==7 || i==13) {
             av.key_frame_draw("num", Pos(0,0), num, {{{"mark"},AV::AtoB(0,i)}, {{"highlight"},{i}}, {{"point"},{i}}, {{"focus"},{i}}, {{"background"},{i}}}, {0},  "normal", 0, 1);
-            av.key_frame_draw("heap", Pos("num","bottom-left",0,100), num, {{{"mark"},AV::AtoB(0,i)}, {{"highlight"},{i-1}}, {{"point"},{i-1}}, {{"focus"},{i-1}}, {{"background"},{i-1}}}, {0},  "heap", 10, 1);
-            av.key_frame_draw("BIT", Pos("heap","bottom-left",0,100), num, {{{"mark"},AV::AtoB(0,i)}, {{"highlight"},{i-1}}, {{"point"},{i-1}}, {{"focus"},{i-1}}, {{"background"},{i-1}}}, {0},  "BIT", 10, 1);
+            av.key_frame_draw("heap", Pos("num","raw bottom-left",0,100), num, {{{"mark"},AV::AtoB(0,i)}, {{"highlight"},{i-1}}, {{"point"},{i-1}}, {{"focus"},{i-1}}, {{"background"},{i-1}}}, {0},  "heap", 10, 1);
+            av.key_frame_draw("BIT", Pos("heap","raw bottom-left",0,100), num, {{{"mark"},AV::AtoB(0,i)}, {{"highlight"},{i-1}}, {{"point"},{i-1}}, {{"focus"},{i-1}}, {{"background"},{i-1}}}, {0},  "BIT", 10, 1);
         }
+        av.auto_camera();
         av.end_frame_draw();
     }
     av.end_draw();
