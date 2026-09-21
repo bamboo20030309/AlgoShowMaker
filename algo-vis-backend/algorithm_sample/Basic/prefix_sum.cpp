303044b74545746dc91980695c166a414ea8cf27
bamboo20030309 <bamboo20030309a@gmail.com>
2026-09-22 02:02:43 +0800

    modernize one-dimensional prefix sum sample

--- algo-vis-backend/algorithm_sample/Basic/prefix_sum.cpp
+++ algo-vis-backend/algorithm_sample/Basic/prefix_sum.cpp
@@ -1,59 +1,66 @@
-//prefix_sum Sample
+// One-dimensional Prefix Sum Sample
 #include <bits/stdc++.h>
-#include "AV.hpp"
 using namespace std;
-AV av;
+
+int n;
+vector<int> num, pre;
+
+// @defaults
+// @camera auto zoom(1.05)
+// @enddefaults
+
+// num[0] 與 pre[0] 是保留格，實際資料從 index 1 開始。
+// @preset prefix_sum_view
+// @object num with labels(value,index)
+// @object pre with labels(value,index)
+// @style num[0] background AV_grey
+// @style pre[0] background AV_grey
+// @place num.left-bottom at pre.left-top offset(0,-70)
+// @endpreset
 
 int main() {
-    int n; cin>>n;
-    vector<int> num(n),pre(n);
-    for(auto&v:num)cin>>v;
-    //draw{
-    vector<int> _draw_focus;
-    av.start_draw();
-    av.start_frame_draw();
-    av.frame_draw("num" , Pos(0,0),   num, {}, {0}, "normal", 0, 1);
-    av.frame_draw("pre" , Pos(0,150),   pre, {}, {0}, "normal", 0, 1);
-    av.auto_camera();
-    av.end_frame_draw();
-    //}
-    pre[0]=num[0];
-    //draw{
-    _draw_focus.push_back(0);
-    av.start_frame_draw();
-    av.frame_draw("num" , Pos(0,0),   num, {{{"highlight"},{0}},{{"focus"},_draw_focus},{{"background","rgba(47, 255, 82, 0.44)"},{0}}}, {0}, "normal", 0, 1);
-    av.frame_draw("pre" , Pos(0,150),   pre, {{{"highlight"},{0}},{{"focus"},{0}},{{"background","rgba(47, 255, 82, 0.44)"},{0}}}, {0}, "normal", 0, 1);
-    av.auto_camera();
-    av.end_frame_draw();
-    //}
-    for(int i=1;i<n;i++){
-        pre[i]=pre[i-1] + num[i];
-        //draw{
-        _draw_focus.push_back(i);
-        av.start_frame_draw();
-        av.frame_draw("num" , Pos(0,0),   num, {{{"highlight"},{i}},{{"focus"},_draw_focus},{{"background","rgba(47, 255, 82, 0.44)"},AV::AtoB(0,i)}}, {0}, "normal", 0, 1);
-        av.frame_draw("pre" , Pos(0,150),   pre, {{{"highlight"},{i-1,i}},{{"focus"},{i}},{{"background","rgba(47, 255, 82, 0.44)"},{i}}}, {0}, "normal", 0, 1);
-        av.auto_camera();
-        av.end_frame_draw();
-        //}
+    cin >> n;
+    num.resize(n + 1);
+    pre.resize(n + 1);
+    for (int i = 1; i <= n; i++) cin >> num[i];
+
+    // @frame use prefix_sum_view
+    // @events animate off
+    // @text "pre[0] 保留為 0；接著從左到右累加每個數字" at num.top offset(0,-20)
+
+    for (int i = 1; i <= n; i++) {
+        pre[i] = pre[i - 1] + num[i];
+
+        // @frame use prefix_sum_view
+        // @style num[1:i] focus
+        // @style pre[0:i] focus
+        // @style num[i] highlight
+        // @style pre[i-1:i] highlight
+        // @style num[1:i] background AV_green
+        // @style pre[i] background AV_green
+        // @text "pre[${i}] = pre[${i - 1}] + num[${i}] = ${pre[i]}" at num.top offset(0,-20)
     }
-    //draw{
-    av.start_frame_draw();
-    av.frame_draw("num" , Pos(0,0),   num, {}, {0}, "normal", 0, 1);
-    av.frame_draw("pre" , Pos(0,150),   pre, {}, {0}, "normal", 0, 1);
-    av.auto_camera();
-    av.end_frame_draw();
-    //}
-    int L,R;
-    // sum of L to R 
-    L=3,R=14;
-    //draw{
-    av.start_frame_draw();
-    av.frame_draw("num" , Pos(0,0),   num, {}, {0}, "normal", 0, 1);
-    av.frame_draw("pre" , Pos(0,150),   pre, {}, {0}, "normal", 0, 1);
-    av.auto_camera();
-    av.end_frame_draw();
-    av.end_draw();
-    //}
+
+    // @frame use prefix_sum_view
+    // @text "一維前綴和建構完成；pre[i] 是前 i 個數字的總和" at num.top offset(0,-20)
+
+    int L, R;
+    cin >> L >> R;
+
+    // @frame use prefix_sum_view
+    // @style num[L:R] background AV_green
+    // @style pre[R] background AV_green
+    // @style pre[L-1] background AV_red
+    // @text "第 ${L} 到第 ${R} 個數字的總和 = pre[${R}] - pre[${L - 1}]" at num.top offset(0,-20)
+
+    int ans = pre[R] - pre[L - 1];
+
+    // @frame use prefix_sum_view
+    // @style num[L:R] background AV_green
+    // @style pre[R] background AV_green
+    // @style pre[L-1] background AV_red
+    // @text "區間和 = ${pre[R]} - ${pre[L - 1]} = ${ans}" at num.top offset(0,-20)
+
+    cout << "sum of L to R = " << ans << '\n';
     return 0;
 }
